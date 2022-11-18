240c862bf58231f028bf8ed373d8c68aecea101d
LotHo33 <godzlotho@gmail.com>
2022-11-18 22:45:17 +0100

    flutter piipo con zanardi pippo

--- ski_time_flutter/lib/main.dart
+++ ski_time_flutter/lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
+void main() => runApp(const App());
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Prova")),
+        appBar: AppBar(title: const Text("Prova")),
+        body: SizedBox(width: 250, child:
+          Column(crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [FloatingActionButton.small(onPressed: null)],)),
       ),
     );
   }
